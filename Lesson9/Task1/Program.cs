9ba57208fe43389e07ef8beaa09b4a1e41503111
Watrushechka <lolodilnik@gmail.com>
2023-07-16 22:36:32 +0400

    ДЗ 9

---
+++ Lesson9/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/**Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+**/
+
+int number = 0;
+
+Console.WriteLine("Введите число : ");
+number = Convert.ToInt32(Console.ReadLine());
+
+void FindNatural(int number1)
+{
+    Console.Write(number1);
+    if (number1 <= 1) return;
+    FindNatural(number1 - 1);
+}
+
+FindNatural(number);
+
+
+
+
---
+++ Lesson9/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿/**Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+**/
+
+int sum = 0;
+int numberMAX=0;
+int numberMin=0;
+
+Console.WriteLine("Введите число первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void FindSUM(int numberMAX)
+{
+    if (numberMAX<numberMin) return;
+
+    sum = sum + numberMAX;
+    FindSUM(numberMAX - 1);
+    
+}
+
+void SerachMAx(int number3, int number4)
+{
+    if (number3 > number4)
+    {
+        numberMAX = number3;
+        numberMin = number4;
+    }
+    else  
+        numberMAX = number4;
+        numberMin = number3;
+}
+
+SerachMAx(number1, number2);
+FindSUM(numberMAX);
+Console.Write(sum);
+
+
+
+
+
+
+
